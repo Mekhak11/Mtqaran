c1d74c4ffa82688edf556399cbf674b827b334e1
MekhakGhapantsyan <mekhakghapantsyan@gmail.com>
2024-09-02 14:59:50 +0400

    Initial Commit

---
+++ Mtqaran/MtqaranApp.swift
@@ -0,0 +1,17 @@
+//
+//  MtqaranApp.swift
+//  Mtqaran
+//
+//  Created by MEKHAK GHAPANTSYAN on 02.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MtqaranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
